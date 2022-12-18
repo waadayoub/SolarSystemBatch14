a0e58396f2f43a01dbdc04ad992de235c3ec052f
waadayoub <113552101+waadayoub@users.noreply.github.com>
2022-12-18 09:22:16 -0600

    The basic structure of Solar System

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Basic structure is ready. Please write down your code in this "+" project");
+    }
+}
